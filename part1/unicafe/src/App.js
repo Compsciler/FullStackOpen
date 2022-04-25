0681fa6c644134c6b239f8fcd6c59f6a45ce812b
Roger Wang <rogerwang2520@gmail.com>
2022-04-25 17:34:55 -0500

    Complete exercises 1.6-1.10

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({good, neutral, bad}) => {
+  const response_count = good + neutral + bad
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={response_count} />
+      <StatisticLine text="average" value={(good - bad) / response_count} />
+      <StatisticLine text="positive" value={(good / response_count).toLocaleString("en", {style: "percent"})} />
+    </div>
+  )
+}
+
+const StatisticLine = ({text, value}) => (
+  <p>
+    {text} {value}
+  </p>
+)
+
+export default App
