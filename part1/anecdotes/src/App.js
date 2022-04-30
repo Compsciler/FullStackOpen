1c52e06f961f7b815aea94425300d97360fefc4b
Roger Wang <rogerwang2520@gmail.com>
2022-04-30 00:10:02 -0500

    Complete exercises 1.12-1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const setVoteCount = (anecdoteIndex, voteCount) => {
+    const votes_copy = [...votes]
+    votes_copy[anecdoteIndex] = voteCount
+    setVotes(votes_copy)
+  }
+
+  const getMostVotedAnecdoteIndex = () => {
+    const max = Math.max(...votes)
+    return votes.indexOf(max)
+  }
+
+  const mostVotedAnecdoteIndex = getMostVotedAnecdoteIndex()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} voteCount={votes[selected]} />
+      <Button onClick={() => setVoteCount(selected, votes[selected] + 1)} text="vote" />
+      <Button onClick={() => setSelected((selected + 1) % anecdotes.length)} text="next anecdote" />
+    
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={anecdotes[mostVotedAnecdoteIndex]} voteCount={votes[mostVotedAnecdoteIndex]} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Anecdote = ({anecdote, voteCount}) => (
+  <>
+    <p>{anecdote}</p>
+    <VoteCount voteCount={voteCount} />
+  </>
+)
+
+const VoteCount = ({voteCount}) => (
+  <p>has {voteCount} votes</p>
+)
+
+export default App
